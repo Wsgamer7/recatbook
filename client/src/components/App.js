40524890957be7262707e326f458c2d1e36ccba7
Shuo Wang <ws18022@outlook.com>
2023-10-03 09:59:21 +0800

    add: story, comment and navbar style

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Router } from "@reach/router";
 
 import Home from "./pages/Home";
+import NavBar from "./modules/Navbar";
 import "./App.css";
 import "../utilities.css";
 const App = () => {
   return (
     <div className="App-container">
-      <Home />
+      <NavBar userId="null" />
+      <Router>
+        <Home path="/" />
+      </Router>
     </div>
   );
 };
