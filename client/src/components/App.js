7b3bbc783db180a7bbf3ba6629744976aa7d2c2b
Shuo Wang <ws18022@outlook.com>
2023-10-04 16:53:21 +0800

    fix props stream, add get story and comment front

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,15 +5,18 @@ import Home from "./pages/Home";
 import NavBar from "./modules/Navbar";
 import "./App.css";
 import "../utilities.css";
+
+const hardUserId = "01";
 const App = () => {
+  const userId = hardUserId;
   return (
     <div>
       <div className="App-NavBarContainer">
-        <NavBar userId="null" />
+        <NavBar userId={userId} />
       </div>
       <div className="App-NavBarSpacer" />
       <Router>
-        <Home path="/" />
+        <Home path="/" userId={userId} />
       </Router>
     </div>
   );
--- client/src/components/modules/Card.tsx
+++ client/src/components/modules/Card.tsx
@@ -1,41 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import Comment from "./Comment";
-import Story from "./Story";
+import { SingleComment } from "./SingleComment";
+import { SingleStory } from "./SingleStory";
 import "./Card.css";
 import { NewComment } from "./NewPostInput";
+import { Story } from "../modules/SingleStory";
+import { Comment } from "../modules/SingleComment";
+import { get } from "../../utilities";
 
-import { Comment_type, Story_type } from "../../../../type";
 /**
  * @param story, story object
  * @param comments, a array of comment object
  * @param userId
  */
+
 type CardProps = {
-  story: Story_type;
-  comments: Comment_type[];
+  story: Story;
   userId: string;
 };
 const Card = (props: CardProps) => {
-  const story = props.story;
-  const commentsArray2Component = (comments: Comment_type[]) => {
-    return comments.map((comment: Comment_type) => (
-      <div>
-        <Comment
-          create_id={comment.create_id}
-          create_name={comment.create_name}
-          content={comment.content}
-        />
-      </div>
-    ));
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [commentJSXs, setCommentJSXs] = useState<JSX.Element[] | null>(null);
+  const story: Story = props.story;
+  useEffect(() => {
+    get("/api/comment", { story_id: story._id }).then((commentObjs: Comment[]) => {
+      setComments(commentObjs);
+    });
+  }, []);
+  const addNewComment = (comment: Comment) => {
+    setComments([comment, ...comments]);
   };
-  const comment_components = commentsArray2Component(props.comments);
+  useEffect(() => {
+    const haveComment = comments.length > 0;
+    if (haveComment) {
+      setCommentJSXs(
+        comments.map((comment: Comment) => (
+          <SingleComment
+            create_id={comment.create_id}
+            create_name={comment.create_name}
+            content={comment.content}
+          />
+        ))
+      );
+    }
+  }, [comments]);
   return (
     <div className="Card-container">
-      <Story create_id={story.create_id} create_name={story.create_name} content={story.content} />
-      <NewComment />
-      <hr />
-      {comment_components}
+      <SingleStory
+        create_id={story.create_id}
+        create_name={story.create_name}
+        content={story.content}
+      />
+      {props.userId && <NewComment />}
+      {comments.length > 0 && <hr />}
+      {commentJSXs}
     </div>
   );
 };
